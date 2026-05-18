a8e4223bb16200266b9add86092ae227a8d315d5
josephyzz <miguelneto275@gmail.com>
2026-05-18 20:08:51 -0300

    Create irmaos.py

---
+++ 2020/level01/irmaos.py
@@ -0,0 +1,18 @@
+"""
+Irmãos
+"""
+# https://olimpiada.ic.unicamp.br/pratique/ps/2020/f1/irmaos/
+# # Points 100/100
+# t = 0,026 / 0,1s
+# memory 3.99 MB/ 64 MB
+
+# Record = 3min
+
+# By josephyzz
+
+n = int(input())
+m = int(input())
+
+r = m - n
+
+print(m + r)
